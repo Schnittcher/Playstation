5bf2a4d5f1e77e1e42a5add0a05d4fcee003fd85
Kai Schnittcher <kai.schnittcher@gmail.com>
2018-03-31 20:26:07 +0000

    Apply fixes from StyleCI

--- libs/PSStore.php
+++ libs/PSStore.php
@@ -1,7 +1,8 @@
-<?
+<?php
 
-class PSStore {
-    private $URL = "https://store.playstation.com/store/api/chihiro/00_09_000/titlecontainer/DE/de/999/";
+class PSStore
+{
+    private $URL = 'https://store.playstation.com/store/api/chihiro/00_09_000/titlecontainer/DE/de/999/';
     private $GameID;
 
     private $AgeLimit;
@@ -15,22 +16,22 @@ class PSStore {
 
     private $StarRating;
 
-
-
-    public function __construct($GameID) {
+    public function __construct($GameID)
+    {
         $this->GameID = $GameID;
         $this->getData();
     }
 
-    private function getData() {
+    private function getData()
+    {
         $ch = curl_init();
         $timeout = 5;
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-        curl_setopt($ch, CURLOPT_URL, $this->URL.$this->GameID."_00");
+        curl_setopt($ch, CURLOPT_URL, $this->URL . $this->GameID . '_00');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13");
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13');
         $file_content = curl_exec($ch);
         curl_close($ch);
 
@@ -38,11 +39,11 @@ class PSStore {
 
         $this->AgeLimit = $obj->age_limit;
         $this->RatingImage = $obj->content_rating->url;
-        $this->GameName =$obj->name;
+        $this->GameName = $obj->name;
         $this->LongDesc = $obj->long_desc;
         $this->Picture = $obj->images[0]->url;
         $this->ReleaseDate = $obj->release_date;
-        $this->ProviderName =  $obj->provider_name;
+        $this->ProviderName = $obj->provider_name;
         $this->StarRating = $obj->star_rating;
     }
 
@@ -117,9 +118,4 @@ class PSStore {
     {
         return $this->StarRating;
     }
-
-
-
 }
-
-?>
